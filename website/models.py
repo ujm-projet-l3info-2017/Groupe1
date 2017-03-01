ad99d3e2de314ca423953c9b549923e5ffb2050b
Paul Viallard <paul.viallard@gmail.com>
2017-03-01 20:03:10 +0100

    feat: models added

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    #id (ajoute automatiquement)
+    intitule = models.TextField()
+    requete = models.TextField()
+
+class Contient(models.Model):
+    #id
+    idQuestion = models.IntegerField()
+    idTable = models.IntegerField()
+
+class Table(models.Model):
+    #id
+    nom = models.CharField(max_length=100)
+    attribut = models.TextField()
+    remplissage = models.TextField()

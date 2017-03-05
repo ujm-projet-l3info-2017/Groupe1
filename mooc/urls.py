a6577800ef5c317ca34abd50288ba9881a0f1da8
JeanRomain <peuriere.romain@outlook.fr>
2017-03-05 13:18:16 +0100

    feat: display of observed request

--- mooc/urls.py
+++ mooc/urls.py
@@ -20,5 +20,6 @@ from website import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^request/', views.request),
     
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.db import connection
 # Create your views here.
 
 def index(request):
@@ -8,3 +9,20 @@ def index(request):
     context={}
     return HttpResponse(template.render(context, request))
 
+def request(request):
+    # Ajouter un argument "contenu requete"
+    # Recupere a l'nevoie de la requete par l'utilisateur
+    
+    template = loader.get_template('website/request.html')
+    with connection.cursor() as cursor:
+        
+        cursor.execute('SELECT * FROM website_contient')
+
+        column_name = [col[0] for col in cursor.description]
+        row = cursor.fetchall()
+    context= {
+        'row': row,
+        'column_name': column_name
+    }
+    return HttpResponse(template.render(context, request))
+    

aa999046351dcef45331b0e5f278415d414238c6
Paul Viallard <paul.viallard@gmail.com>
2017-03-05 11:56:47 +0100

    feat: view.py modified for javascript and style

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
+
+def javascript(request, javascript_file):
+    return HttpResponse(loader.get_template('website/js/'+javascript_file).render(None, request))
+
+def style(request, style_file):
+    return HttpResponse(loader.get_template('website/css/'+style_file).render(None, request))
 
 def index(request):
     template = loader.get_template('website/index.html')

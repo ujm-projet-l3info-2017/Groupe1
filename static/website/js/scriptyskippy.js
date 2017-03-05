4056fd4d8641aeba33291ff43c55db0c13614167
hugo <hugo@francis.home>
2017-03-05 15:40:15 +0100

    feat: adding ajax first steps

--- static/website/js/scriptyskippy.js
+++ static/website/js/scriptyskippy.js
@@ -1,3 +1,22 @@
-function sayHello() {    
+function get_token(){
+    var cookies=document.cookie.split(";");
+    var token;
+    for (var i = 0 ; i < cookies.length ; i++) {
+	cookies[i]=cookies[i].split('=');
+	if (cookies [i][0] === "csrftoken" )
+	    token = cookies [i][1];
+    }
+    return(token);
+}
+function post_ajax() {    
     $("#output_request").html( "Next Step..." );   
+    get_token();
+    $.ajax({
+	url : '/request/', 
+	type : 'POST' ,
+	headers: {
+            'X_CSRF_TOKEN': get_token()
+	},
+    });
+    
 }

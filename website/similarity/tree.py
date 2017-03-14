4a64df82de25cf32fadb5cab1905bf6a6167e87f
Guillaume Vidot <eric.guillaume.vidot@hotmail.fr>
2017-03-14 16:12:04 +0100

    feat: two function (rotate left and rotate right)

--- website/similarity/tree.py
+++ website/similarity/tree.py
@@ -1,7 +1,32 @@
 class Tree:
     left = None
     right = None
+    element = None
+    balance = 0
 
     def __init__(self, l, r):
-        left = l
-        right = r
+        self.left = l
+        self.right = r
+
+    def rotate_left(self, father):
+        tree_tmp_left = self
+        if(father.right == self):
+            father.right = self.right
+        else:
+            father.left = self.right
+        tree_tmp_left.right = self.left
+        self.left = tree_tmp_left
+
+    def rotate_right(self, father):
+        tree_tmp_right = self
+        if(father.right == self):
+            father.right = self.left
+        else:
+            father.left = self.left
+        tree_tmp_right.left = self.right
+        self.right = tree_tmp_right
+
+    def rotate_right_left(self, father):
+        
+            
+        

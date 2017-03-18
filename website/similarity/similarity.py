90d9828d7edf7dd47f3c564f122fed8771deb080
Guillaume Vidot <eric.guillaume.vidot@hotmail.fr>
2017-03-18 16:17:07 +0100

    feat: add the function to compare two table
    the function compare a table whit a tree.

--- website/similarity/similarity.py
+++ website/similarity/similarity.py
@@ -45,6 +45,39 @@ def convert_table_tree(row, column_name):
         table[column_name[i]] = convert_column_tree(column)
     return table
 
+def compare_table(table_expected, table_result, column_expected, column_result):
+    GREEN = 0
+    ORANGE = 1
+    RED = 2
+    length_expected = len(column_expected)
+    color_column = []
+    color_table = [][]
+    trees = convert_table_tree(table_expected, column_expected)
+    # Attribute a color to a column :
+    #    GREEN if it's ok
+    #    ORANGE if it's not at the right place
+    for i in range(len(column_result)):
+        if(i < length_expected and column_expected[i] == column_result[i]):
+            color_column[i] = GREEN
+        else:
+            color_column[i] = ORANGE
+    # Attribute a color at each cellule of the table
+    for i in range(len(table_result)):
+        for j in range(len(table_result[i])):
+            elem = table_result[i][j]
+            try:
+                # Use the right tree
+                tree = trees[column_result[j]]
+                # Check if the element exist in the tree
+                if(tree[elem] != None):
+                    color_table[i][j] = GREEN
+                else:
+                    color_table[i][j] = RED
+            except KeyError:
+                # The column doesn't exist
+                color_column[i] = RED
+            
+
 column_name = ["C1", "C2", "C3", "C4"]
 row = [["1", "3", "A", "B"],
        ["2", "4", "7", "D"],

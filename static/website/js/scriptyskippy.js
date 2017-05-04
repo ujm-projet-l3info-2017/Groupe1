ea504b3a81f75c121bb27d9f1fb6cb27ef0a2c21
Guillaume Vidot <eric.guillaume.vidot@hotmail.fr>
2017-05-04 18:16:01 +0200

    feat: continue editor syntax coloration

--- static/website/js/scriptyskippy.js
+++ static/website/js/scriptyskippy.js
@@ -109,3 +109,67 @@ function get_tables() {
 	}
     });   
 }
+
+function get_length() {
+    var e = document.getElementById("input_text");
+    var length = 0;
+    var i=0;
+    while(i<e.childNodes.length &&  e.childNodes.item(i).childNodes.item(0) != null && e.childNodes.item(i).childNodes.item(0) != window.getSelection().focusNode) {
+	length += e.childNodes.item(i).childNodes.item(0).nodeValue.length;
+	i++;
+    }
+    length += window.getSelection().getRangeAt(0).startOffset;
+    return length;
+}
+
+function set_cursor(length) {
+    var e = document.getElementById("input_text");
+    for(var i=0; i<e.childNodes.length; i++) {
+	var n_e = e.childNodes.item(i).childNodes.item(0);
+	var n = n_e.nodeValue;
+	var n_length = n.length;
+	if(n_length >= length) {
+	    var range = document.createRange();
+	    range.setStart(n_e,length);
+	    window.getSelection().removeAllRanges();
+	    window.getSelection().addRange(range);
+	}
+	else {
+	    length -= n_length;
+	}
+    }    
+}
+
+function color(){
+    input_text = document.getElementById("input_text");
+    var sentence = input_text.innerText;
+    var p = new SQLSyntaxParser(sentence);
+    var last_letter = sentence[sentence.length-1];
+    l = get_length();
+    
+    try{
+	p.parse();
+    }catch(e){}
+    
+    var tabcolor = p.highlighting;
+    input_text.innerHTML = "";
+    console.log(tabcolor);
+    for(var i=0; i< tabcolor.length; i++){
+	var span = document.createElement("span");
+	span.innerText = tabcolor[i][0];
+	span.style.color = tabcolor[i][1];
+	input_text.appendChild(span);
+	if(i < tabcolor.length-1) {
+	    var span = document.createElement("span");
+	    span.innerHTML = "&nbsp;";
+	    input_text.appendChild(span);
+	}
+    }
+    if(last_letter == '\n') {
+	var span = document.createElement("span");
+	span.innerHTML = "&nbsp;";
+	input_text.appendChild(span);
+    }
+    
+    set_cursor(l);
+}

5f251dd337d540fbc393d4356860b12c462dff52
Paul Viallard <paul.viallard@gmail.com>
2017-05-15 19:10:28 +0200

    feat: add comment in view.py

--- website/views.py
+++ website/views.py
@@ -5,10 +5,16 @@ from django.db import connection
 from .similarity.similarity import compare_table
 from .similarity.mapping import Mapping
 from .parser.syntax import SQLSyntaxParser
-# Create your views here.
+
+#===================================================#
+#                        HTML                       #
+#===================================================#
 
 def index(request):
-    #load_tables()
+    """
+    The view for the index page
+    We load the other view to print the index page
+    """
     template = loader.get_template('website/index.html')
     exercice = load_exercise(request).content
     question = load_question(request).content
@@ -25,22 +31,14 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def request(request):
-    # Ajouter un argument "contenu requete"
-    # Recupere a l'envoi de la requete par l'utilisateur
-
+    """
+    Execute the request of the user to load the result 
+    in the request page
+    """
     requete = request.POST.get('query')
     
     column_expected, table_expected = expected_request(request)
     template = loader.get_template('website/request.html')
-
-    
-    #check=check_table(request)
-    #if (check==0) :
-    #    context= {   
-    #        'error': "no such table"
-    #    }
-    #    template = loader.get_template('website/error_request.html')
-#    return HttpResponse(template.render(context, request))
     
     try:
         p = SQLSyntaxParser(requete)
@@ -73,81 +71,18 @@ def request(request):
     return HttpResponse(template.render(context, request))
 
 
-def check_table(request):
-    # Returns 1 if all tables are accessible in the request
-    requete = request.POST.get('query')
-    requete = requete.lower()
-
-    requete = requete.replace(";","")
-    
-    exercice_no  = request.POST.get('exercise_no')
-    if(exercice_no == None):
-        exercice_no = "1"
-
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute("SELECT nom FROM website_table,website_exercice,website_contient_exercice_table WHERE website_exercice.numero="+exercice_no+" AND website_exercice.id=idExercice AND idTable=website_table.id")
-            row = cursor.fetchall()
-            table_list=[]
-            for line in row:
-                for l in line:
-                    table_list.append(str(l))
-                    print(str(l))
-        except:
-            print("Probleme check table")
-
-    first_split=requete.split("from")
-    second_split=first_split[1].split("where")
-    second_split[0]=second_split[0].replace(" ","")
-    tables=second_split[0].split(",")
-
-    for item in tables:
-        print(" TEST: "+item)
-        if item not in table_list:
-            return 0
-    return 1
-
-def expected_request(request):
-    exercice_no  = request.POST.get('exercise_no')
-    if(exercice_no == None):
-        exercice_no = "1"
-    question_no  = request.POST.get('question_no')
-    if(question_no == None):
-        question_no = "1"
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute("select requete from website_question,website_contient_exercice_question,website_exercice where website_question.numero="+question_no+" AND website_question.id=idQuestion AND idExercice=website_exercice.id AND website_exercice.numero="+exercice_no)
-            expected_request = str(cursor.fetchone()[0])
-
-        except Exception as e:
-            error = str(e)
-            context= {
-                'error': error
-            }
-            template = loader.get_template('website/error_request.html')
-        try:
-            print(expected_request)
-            cursor.execute(expected_request)
-            column_name = [col[0] for col in cursor.description]
-            row = cursor.fetchall()
-            row = [[str(row[i][j]) for j in range(len(row[i]))] for i in range(len(row))]
-            return (column_name,row)
-        except Exception as e:
-            error = str(e)
-            context= {
-                'error': error
-            }
-            template = loader.get_template('website/error_request.html')
-
 def load_hint(request):
+    """
+    Load some hints according to the user query, the exercise number and the question number
+    """
     user_request = request.POST.get('query')
     # We take the expected request
     exercice_no  = request.POST.get('exercise_no')
     if(exercice_no == None):
-        exercice_no = "1"
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
     question_no  = request.POST.get('question_no')
     if(question_no == None):
-        question_no = "1"
+        question_no = "(SELECT id FROM website_question ORDER BY id ASC LIMIT 1)"
     with connection.cursor() as cursor:
         try:
             cursor.execute("select requete from website_question,website_contient_exercice_question,website_exercice where website_question.numero="+question_no+" AND website_question.id=idQuestion AND idExercice=website_exercice.id AND website_exercice.numero="+exercice_no)
@@ -183,6 +118,9 @@ def load_hint(request):
     
             
 def load_expected_request(request):
+    """
+    Print the expected request
+    """
     column_name,row=expected_request(request)
         
     template = loader.get_template('website/expected_request.html')
@@ -192,60 +130,17 @@ def load_expected_request(request):
     }    
     return HttpResponse(template.render(context, request))
 
-def load_tables(request):
-    # On charge les donnees de l'exercice > a passer en argument POST (formulaire)
-    
-    exercice_no  = request.POST.get('exercise_no')
-    if(exercice_no == None):
-        exercice_no = "1"
-
-    print("DAns load tables: "+exercice_no)
-
-        
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute("SELECT * FROM website_table,website_contient_exercice_table,website_exercice WHERE website_exercice.numero="+exercice_no+" AND website_exercice.id=idExercice AND idTable=website_table.id")
-            row=cursor.fetchall()
-            for line in row:
-                tableau=[]
-                for l in line:
-                    tableau.append(l)
-                tableau[0]=str(tableau[0])
-                tableau[1]=str(tableau[1]) # NOM de la table
-                tableau[2]=str(tableau[2]) # attributs de creation
-                tableau[3]=str(tableau[3]) # Insert into
-                cursor.execute('CREATE TABLE '+tableau[1]+' '+tableau[2])
-                tableau[3]=tableau[3].split('\n')
-                for insertline in tableau[3]:
-                    cursor.execute('INSERT INTO '+tableau[1]+" "+insertline)
-        except Exception as e:
-            print("Erreur: "+str(e))
-
-
-def drop_tables(request):
-    #supprime toutes les tables de l'exo, une amelio serait de chopper l'ancien num d'exo
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute("SELECT nom FROM website_table")
-            row=cursor.fetchall()
-            for line in row:
-                for l in line:
-                    cursor.execute('DROP TABLE IF EXISTS '+str(l))
-        except Exception as e:
-            print("Erreur: "+str(e))
-            return HttpResponse(status=400)
-    #je charge les bonnes tables maintenant
-    load_tables(request)
-
-    return HttpResponse(status=200)
 
 def load_label(request):
+    """
+    Load the question label
+    """
     exercice_no  = request.POST.get('exercise_no')
     if(exercice_no == None):
-        exercice_no = "1"
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
     question_no  = request.POST.get('question_no')
     if(question_no == None):
-        question_no = "1"
+        question_no = "(SELECT id FROM website_question ORDER BY id ASC LIMIT 1)"
     requete = "SELECT intitule FROM website_question,website_contient_exercice_question,website_exercice WHERE website_question.id = website_contient_exercice_question.idQuestion AND website_exercice.id = website_contient_exercice_question.idExercice AND website_exercice.id ="+exercice_no+" AND website_question.id ="+question_no
     with connection.cursor() as cursor:
         try:
@@ -262,6 +157,9 @@ def load_label(request):
         return  HttpResponse("<p>"+ row +"</p>")
 
 def load_question(request):
+    """
+    We load the question, we drop the older tables
+    """
     template = loader.get_template('website/question.html')
     exercice_no  = request.POST.get('exercise_no')
     if(exercice_no == None):
@@ -269,16 +167,9 @@ def load_question(request):
     requete = "SELECT website_question.numero FROM website_contient_exercice_question,website_question,website_exercice WHERE website_exercice.id=website_contient_exercice_question.idExercice AND website_question.id=website_contient_exercice_question.idQuestion AND website_exercice.numero="+exercice_no
 
 
-    # On enleve les autres tables
+    # We drop the other tables
     drop_tables(request)
-    # On charge les tables de l'exercice
-
-    #######################################################################################################
-    #
-    # /!\ CHARGE 2 FOIS toutes les fonctions: au clic du menu deroulant, au clic du choix
-    # > Devrait faire qu'au choix.
-    #
-    #######################################################################################################
+    # We load the exercise tables
     
     with connection.cursor() as cursor:
         try:
@@ -298,6 +189,9 @@ def load_question(request):
         return HttpResponse(template.render(context, request))
 
 def load_exercise(request):
+    """
+    Load the exercise
+    """
     template = loader.get_template('website/exercise.html')
     
     requete = "SELECT numero FROM website_exercice"
@@ -319,10 +213,13 @@ def load_exercise(request):
     return HttpResponse(template.render(context, request))
 
 def load_tables_exercise(request):
+    """
+    We load the tables available in the exercise
+    """
     template = loader.get_template('website/tables_exercise.html')
     exercice_no  = request.POST.get('exercise_no')
     if(exercice_no == None):
-        exercice_no = "1"
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
     requete = "SELECT website_table.nom FROM website_table, website_exercice, website_contient_exercice_table WHERE website_contient_exercice_table.idExercice=website_exercice.numero AND website_contient_exercice_table.idtable=website_table.id AND numero="+exercice_no
    
     with connection.cursor() as cursor:
@@ -352,3 +249,125 @@ def load_tables_exercise(request):
             return HttpResponse(template.render(context, request))
 
     return HttpResponse(template.render(context, request))
+
+#===================================================#
+#                      Database                     #
+#===================================================#
+
+def drop_tables(request):
+    """
+    Drop the exercise tables 
+    """
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute("SELECT nom FROM website_table")
+            row=cursor.fetchall()
+            for line in row:
+                for l in line:
+                    cursor.execute('DROP TABLE IF EXISTS '+str(l))
+        except Exception as e:
+            print("Erreur: "+str(e))
+            return HttpResponse(status=400)
+
+    # And we load the tables
+    load_tables(request)
+    return HttpResponse(status=200)
+
+
+def load_tables(request):
+    """
+    Load the exercice tables according to the exercise number
+    """
+    exercice_no  = request.POST.get('exercise_no')
+    if(exercice_no == None):
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
+
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute("SELECT * FROM website_table,website_contient_exercice_table,website_exercice WHERE website_exercice.numero="+exercice_no+" AND website_exercice.id=idExercice AND idTable=website_table.id")
+            row=cursor.fetchall()
+            for line in row:
+                tableau=[]
+                for l in line:
+                    tableau.append(l)
+                tableau[0]=str(tableau[0])
+                tableau[1]=str(tableau[1]) # NOM de la table
+                tableau[2]=str(tableau[2]) # attributs de creation
+                tableau[3]=str(tableau[3]) # Insert into
+                cursor.execute('CREATE TABLE '+tableau[1]+' '+tableau[2])
+                tableau[3]=tableau[3].split('\n')
+                for insertline in tableau[3]:
+                    cursor.execute('INSERT INTO '+tableau[1]+" "+insertline)
+        except Exception as e:
+            print("Erreur: "+str(e))
+            
+
+def expected_request(request):
+    """
+    Get the expected request according to the exercise number and the question number
+    """
+    exercice_no  = request.POST.get('exercise_no')
+    if(exercice_no == None):
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
+    question_no  = request.POST.get('question_no')
+    if(question_no == None):
+        question_no = "(SELECT id FROM website_question ORDER BY id ASC LIMIT 1)"
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute("select requete from website_question,website_contient_exercice_question,website_exercice where website_question.numero="+question_no+" AND website_question.id=idQuestion AND idExercice=website_exercice.id AND website_exercice.numero="+exercice_no)
+            expected_request = str(cursor.fetchone()[0])
+
+        except Exception as e:
+            error = str(e)
+            context= {
+                'error': error
+            }
+            template = loader.get_template('website/error_request.html')
+        try:
+            cursor.execute(expected_request)
+            column_name = [col[0] for col in cursor.description]
+            row = cursor.fetchall()
+            row = [[str(row[i][j]) for j in range(len(row[i]))] for i in range(len(row))]
+            return (column_name,row)
+        except Exception as e:
+            error = str(e)
+            context= {
+                'error': error
+            }
+            template = loader.get_template('website/error_request.html')
+
+
+def check_table(request):
+    """
+    Check if all tables are accessible in the request 
+    (and return 1 if it's the case)
+    """
+    requete = request.POST.get('query')
+    requete = requete.lower()
+
+    requete = requete.replace(";","")
+    
+    exercice_no  = request.POST.get('exercise_no')
+    if(exercice_no == None):
+        exercice_no = "(SELECT id FROM website_exercice ORDER BY id ASC LIMIT 1)"
+
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute("SELECT nom FROM website_table,website_exercice,website_contient_exercice_table WHERE website_exercice.numero="+exercice_no+" AND website_exercice.id=idExercice AND idTable=website_table.id")
+            row = cursor.fetchall()
+            table_list=[]
+            for line in row:
+                for l in line:
+                    table_list.append(str(l))
+        except Exception as e:
+            print("Erreur: "+str(e))
+
+    first_split=requete.split("from")
+    second_split=first_split[1].split("where")
+    second_split[0]=second_split[0].replace(" ","")
+    tables=second_split[0].split(",")
+
+    for item in tables:
+        if item not in table_list:
+            return 0
+    return 1
